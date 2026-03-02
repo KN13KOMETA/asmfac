dac8bc389546e46803ff7c0d6564d3e010057cbf
KN13KOMETA <kn13kometa@gmail.com>
2026-03-02 16:34:18 +0300

    Prompt uint from user

--- src/start.s
+++ src/start.s
@@ -7,6 +7,12 @@
 
 .section .bss
 
+fac_input:
+  .quad 0
+
+fac_result:
+  .quad 0
+
 buf_input:
   .skip 8
 
@@ -14,38 +20,44 @@ buf_input:
 
 .section .rodata
 
-message:
-  .asciz "ECHO: "
+m_user_input:
+  .ascii "Write positive int for calculating factorial: "
+m_user_input_len = . - m_user_input
 
 
 
 .section .text
 
-_start:
-  #read
+prompt_user_input:
+  # write
+  mov rax, 1
+  mov rdi, 1
+  lea rsi, [m_user_input]
+  mov rdx, m_user_input_len
+  syscall
+
+  # read
   mov rax, 0
   mov rdi, 0
   lea rsi, [buf_input]
   mov rdx, 8
   syscall
 
-  # write
-  mov rax, 1
-  mov rdi, 1
-  lea rsi, [message]
-  mov rdx, 6
-  syscall
-
-  # write
-  mov rax, 1
-  mov rdi, 1
+  # convert ascii to uint
+  mov rdi, 10
   lea rsi, [buf_input]
-  mov rdx, 6
-  syscall
+  mov rdx, 8
+  call atoui
+
+  # move convert result to input
+  mov [fac_input], rax
+  ret
+
+_start:
+  call prompt_user_input
 
   # exit
   mov rax, 60
-  # mov rdi, 0
-  mov rdi, rdx
+  mov rdi, [fac_input]
   syscall
 
